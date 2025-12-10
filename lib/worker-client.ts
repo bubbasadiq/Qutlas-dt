0908693717a6a125a132c134baf5527ed7a336e4
bubbasadiq <114355121+bubbasadiq@users.noreply.github.com>
2025-12-10 19:12:41 +0100

    Add WorkerClient class and WorkerMessage interface

--- lib/worker-client.ts
+++ lib/worker-client.ts
@@ -1,1 +1,34 @@
 
+export interface WorkerMessage {
+  id: string;
+  result?: unknown;
+  error?: string;
+}
+
+export class WorkerClient {
+  private callbacks = new Map<string, (msg: WorkerMessage) => void>();
+  private idCounter = 0;
+
+  constructor(private worker: Worker) {
+    this.worker.onmessage = (e: MessageEvent<WorkerMessage>) => {
+      const { id } = e.data;
+      const cb = this.callbacks.get(id);
+      if (cb) {
+        cb(e.data);
+        this.callbacks.delete(id);
+      }
+    };
+  }
+
+  invoke<T = unknown>(type: string, data?: any): Promise<T> {
+    return new Promise((resolve, reject) => {
+      const id = `${this.idCounter++}`;
+      this.callbacks.set(id, (msg) => {
+        if (msg.error) reject(new Error(msg.error));
+        else resolve(msg.result as T);
+      });
+      this.worker.postMessage({ id, type, data });
+    });
+  }
+}
+
